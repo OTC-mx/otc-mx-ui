c81b4ed064e5c4d100e03c54da553c4d1ad1f07b
khelmy <khelmy@andrew.cmu.edu>
2019-09-29 13:47:31 -0400

    create_option_hook

--- src/hooks/create_option_hook.js
+++ src/hooks/create_option_hook.js
@@ -1,17 +1,50 @@
-import React, { useState } from 'react'; 
-import Web3 from 'web3';
+const React = require('react');
+const Web3 = require('web3');
+const ProviderMappings = require('../utils/provider_mappings');
+const OptionFactory = require('../atomicoptions/build/contracts/option_factory');
 
-let factory_address = '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B';
+const provider = 'http://localhost:8545'
 
-async function create_call_option(issuer, buyer, base_addr, asset_addr, fee,
+const useState = React.useState
+const web3 = new Web3(new Web3.providers.HttpProvider(provider));
+let factory_address = ProviderMappings.factory_mappings[provider];
+let option_factory = new web3.eth.Contract(OptionFactory.abi, factory_address);
+
+async function create_call_option(accounts, issuer, buyer, base_addr, asset_addr, fee,
     strike_price_base, strike_price_quote, volume, maturity_time, expiry_time) {
   let create_option_call = await (option_factory
+    .methods
     .createOption(issuer, buyer,
       base_addr, asset_addr,
       fee, strike_price_base, strike_price_quote,
       volume,
-      maturity_time, expiry_time,
-      { from: accounts[0] })
+      maturity_time, expiry_time)
+    .send({ from: accounts[0] })
   );
-  return;
+  option_address = create_option_call.events.NewOption.returnValues[0];
+  return option_address;
 }
+
+async function create_call_wrapper() {
+  let accounts = await web3.eth.getAccounts()
+
+  // Variables consistent with createOption
+  let issuer = accounts[0];
+  let buyer = accounts[1];
+  let base_addr = ProviderMappings.local_token_mappings['b'];
+  let asset_addr = ProviderMappings.local_token_mappings['a'];
+  let fee = '1' + ('0'.repeat(21));
+  let strike_price_base = 3;
+  let strike_price_quote = 5;
+  let volume = '5' + ('0'.repeat(21));
+  let maturity_time = '0';
+  let expiry_time = '1577836800';
+
+  let option_address = await create_call_option(accounts, issuer, buyer, base_addr, asset_addr, fee,
+      strike_price_base, strike_price_quote, volume, maturity_time, expiry_time);
+
+  console.log(option_address);
+  return option_address;
+}
+
+create_call_wrapper();
---
+++ src/utils/provider_mappings.js
@@ -0,0 +1,14 @@
+// Maps provider-factory
+
+const factory_mappings = ({
+  'http://localhost:8545': '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B',
+  'local': '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B',
+});
+
+const local_token_mappings = ({
+  'a': '0xD833215cBcc3f914bD1C9ece3EE7BF8B14f841bb',
+  'b': '0xD3aA556287Afe63102e5797BFDDd2A1E8DbB3eA5',
+});
+
+module.exports.factory_mappings = factory_mappings;
+module.exports.local_token_mappings = local_token_mappings;
