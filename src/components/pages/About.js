e6a590fef6c032e8961f9816b252c7394ba13daf
khelmy <khelmy@andrew.cmu.edu>
2019-10-10 16:57:46 -0400

    Add link to github

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,8 @@ const About = () => (
   <div>
     <h1>About</h1>
     <p> This app was built by Karim Helmy, an independent analyst in the cryptocurrency space with a focus on mechanism design and protocol analysis. He’s previously consulted with Reciprocal Ventures, Coin Metrics, and Dapper Labs, and recently graduated from Carnegie Mellon University with a degree in Statistics and Machine Learning. You can find out more about Karim <a href="https://karimhelmy.com">here</a>, or drop a line at <a href="mailto:karim@timelock.cc">karim@timelock.cc</a>. </p>
+    <p></p>
+    <p>Source code for this project is available <a href="https://github.com/OTC-mx">here</a>.</p>
   </div>
 );
 
