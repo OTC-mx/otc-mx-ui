f37a755eb9d84fe82965ec5b3f7ed45caff8c9f0
khelmy <khelmy@andrew.cmu.edu>
2019-10-10 17:09:54 -0400

    Add disclaimer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,6 +5,7 @@ const Home = () => (
     <h1>OTC.mx</h1>
     <p>OTC.mx is a platform for trustlessly trading options on ERC20 tokens peer-to-peer.</p>
     <p>Read the <a href="/whitepaper">whitepaper</a> or get started using <a href="/call">call options</a> and <a href="/silentcall">silent call options</a>.</p>
+    <p>Disclaimer: both call options and silent call options are live on Rinkeby testnet. Do not attempt to use them on other networks.</p>
   </div>
 );
 
