19c1e3388d8e796c03cbb3fd7063d49ba08b62b9
khelmy <khelmy@andrew.cmu.edu>
2019-10-22 14:57:49 -0400

    Options are a dropdown menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,15 @@ const Header = () => (
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/whitepaper">Whitepaper</a></li>
-        <li><a href="/call">Call Options</a></li>
-        <li><a href="/silentcall">Silent Call Options</a></li>
+        <li>
+          <div class="dropdown">
+            <button class="dropbtn">Options</button>
+            <div class="dropdown-content">
+              <a href="/call">Call Options</a>
+              <a href="/silentcall">Silent Call Options</a>
+            </div>
+          </div>
+        </li>
         <li><a href="/about">About</a></li>
       </ul>
     </nav>
