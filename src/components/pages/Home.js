c598be13d3a4edc21ab22a82c8795e943a90ab33
khelmy <khelmy@andrew.cmu.edu>
2019-10-26 13:22:16 -0400

    Update Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1>OTC.mx</h1>
-    <p>OTC.mx is a platform for trustlessly trading options on ERC20 tokens peer-to-peer.</p>
-    <p>Read the <a href="/whitepaper">whitepaper</a> or get started using <a href="/call">call options</a> and <a href="/silentcall">silent call options</a>.</p>
-    <p>Disclaimer: both call options and silent call options are live on Rinkeby testnet. Do not attempt to use them on other networks.</p>
+    <p>OTC.mx is a platform for trustlessly trading peer-to-peer derivatives on ERC20 tokens.</p>
+    <p>Read the <a href="/whitepaper">whitepaper</a> or get started using <a href="/call">call options</a> and <a href="/forward">forwards</a>. Or check out <a href="/silentcall">silent call options</a>, <a href="/tokenizedcall">tokenized call options</a>, and <a href="/managedforward">managed forwards</a>.</p>
+    <p>Disclaimer: all contracts are live on Rinkeby testnet. Do not attempt to use them on other networks.</p>
   </div>
 );
 
