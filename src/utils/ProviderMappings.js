e2e88673c1b9b7b97108383e377b2d701adb5beb
khelmy <khelmy@andrew.cmu.edu>
2019-10-27 12:34:50 -0400

    Add rinkeby factory mappings

--- src/utils/ProviderMappings.js
+++ src/utils/ProviderMappings.js
@@ -3,33 +3,37 @@
 const option_factory_mappings = ({
   'http://localhost:8545': '0xCfEB869F69431e42cdB54A4F4f105C19C080A601',
   'private': '0xCfEB869F69431e42cdB54A4F4f105C19C080A601',
-  'rinkeby': '0xafF7c772D52E153C4Dbef94f2B86102bB232F70d',
+  'rinkeby': '0x3822cBa35E3852cAd6216dF77C6a5D5a7161D614',
 });
 
 const silent_option_factory_mappings = ({
   'http://localhost:8545': '0xC89Ce4735882C9F0f0FE26686c53074E09B0D550',
   'private': '0xC89Ce4735882C9F0f0FE26686c53074E09B0D550',
-  'rinkeby': '0x9380Ed33904F2AEd5Cf3d0042CC27Dd2CE49a406',
+  'rinkeby': '0x2bbf14418Ff723306Aee412B61ac16a3DcF2F9Cd',
 });
 
 const tokenized_option_factory_mappings = ({
   'http://localhost:8545': '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7',
   'private': '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7',
+  'rinkeby': '0x82e25dDd36187224c40b0DBA7F487080537bFe4E',
 });
 
 const forward_factory_mappings = ({
   'http://localhost:8545': '0x59d3631c86BbE35EF041872d502F218A39FBa150',
   'private': '0x59d3631c86BbE35EF041872d502F218A39FBa150',
+  'rinkeby': '0xb1A8e083867515de47BA62290E72109623466dF9',
 });
 
 const portfolio_factory_mappings = ({
   'http://localhost:8545': '0x9b1f7F645351AF3631a656421eD2e40f2802E6c0',
   'private': '0x9b1f7F645351AF3631a656421eD2e40f2802E6c0',
+  'rinkeby': '0xaE2c4B48F7DbA7571e4e4e69Ba80F50c9A02532d',
 });
 
 const managed_forward_factory_mappings = ({
   'http://localhost:8545': '0x67B5656d60a809915323Bf2C40A8bEF15A152e3e',
   'private': '0x67B5656d60a809915323Bf2C40A8bEF15A152e3e',
+  'rinkeby': '0x712cB4F4Aac86Eed80D0EF1E9F69CA9F52F8117b',
 });
 
 const local_token_mappings = ({
@@ -38,8 +42,8 @@ const local_token_mappings = ({
 });
 
 const rinkeby_token_mappings = ({
-  'a': '0x86abBC560adBe54CCb2ddfDb58A4Cf11e930396B',
-  'b': '0x900D84D4558b1ef6C2103b361F9aA14ac8Df227A',
+  'a': '0xEa774D3A6Eb116E474E5d723d1Ee34660B4708E5',
+  'b': '0xf70E4D59A9268d777996e00aB41604D40936BD12',
 });
 
 module.exports.option_factory_mappings = option_factory_mappings;
