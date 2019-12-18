13aea277103379bd5535b9bf4325e3c15c61bd8c
khelmy <khelmy@andrew.cmu.edu>
2019-12-18 11:48:55 -0500

    Add warning

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <h1>OTC.mx</h1>
     <p>OTC.mx is a platform for trustlessly trading peer-to-peer derivatives on ERC20 tokens.</p>
     <p>Read the <a href="/whitepaper">whitepaper</a> or get started using <a href="/call">call options</a> and <a href="/forward">forwards</a>. Or check out <a href="/silentcall">silent call options</a>, <a href="/tokenizedcall">tokenized call options</a>, and <a href="/managedforward">managed forwards</a>.</p>
-    <p>Disclaimer: all contracts are live on Rinkeby testnet. Do not attempt to use them on other networks.</p>
+    <p>The complete, unaudited contracts are live on Ethereum mainnet and Rinkeby testnet. Use at your own peril. ☠️‬</p>
   </div>
 );
 
